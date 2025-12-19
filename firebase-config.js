85e6ebab8593ef7cdab418e536f5fa17ef669ec3
Wolbes <arsenmost27@gmail.com>
2025-12-19 10:44:05 +0100

    update v2

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// firebase-config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyBRTYdQtDjgyLEHlooFVyRiFsURI6H_YF0",
+  authDomain: "wolbes-github-io.firebaseapp.com",
+  projectId: "wolbes-github-io",
+  storageBucket: "wolbes-github-io.firebasestorage.app",
+  messagingSenderId: "896477375958",
+  appId: "1:896477375958:web:136ccdf23d267aa243c877"
+};
